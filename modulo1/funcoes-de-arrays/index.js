5a512d90b8a8f72f14fc9700183b9d54261dc63f
Pablo Andrade <pablofla14@hotmail.com>
2022-02-24 17:48:22 -0300

    Funcoes-de-Arrays

---
+++ modulo1/funcoes-de-arrays/index.js
@@ -0,0 +1,142 @@
+/* Exercicio 1
+const usuarios = [
+    { nome: "Amanda Rangel", apelido: "Mandi" },
+    { nome: "Laís Petra", apelido: "Laura" },
+    { nome: "Letícia Chijo", apelido: "Chijo" }
+  ]
+  
+  const novoArrayA = usuarios.map((item, index, array) => {
+     console.log(item, index, array)
+  })
+
+
+Vai ser impresso o nomes e apelidos de todos os itens do array
+
+
+Exercício 2
+const usuarios = [
+  { nome: "Amanda Rangel", apelido: "Mandi" },
+  { nome: "Laís Petra", apelido: "Laura" },
+  { nome: "Letícia Chijo", apelido: "Chijo" },
+]
+
+const novoArrayB = usuarios.map((item, index, array) => {
+   return item.nome
+})
+
+console.log(novoArrayB)
+
+Vai ser impresso somente os nomes sem APELIDOS
+
+Exercício 3
+
+const usuarios = [
+  { nome: "Amanda Rangel", apelido: "Mandi" },
+  { nome: "Laís Petra", apelido: "Laura" },
+  { nome: "Letícia Chijo", apelido: "Chijo" },
+]
+
+const novoArrayC = usuarios.filter((item, index, array) => {
+   return item.apelido !== "Chijo"
+})
+
+console.log(novoArrayC)
+
+Vai ser impresso todos os nomes retirando da "Chijo"
+
+*/
+
+
+//Exercício 1
+
+//a
+const pets = [
+    { nome: "Lupin", raca: "Salsicha"},
+    { nome: "Polly", raca: "Lhasa Apso"},
+    { nome: "Madame", raca: "Poodle"},
+    { nome: "Quentinho", raca: "Salsicha"},
+    { nome: "Fluffy", raca: "Poodle"},
+    { nome: "Caramelo", raca: "Vira-lata"},
+ ]
+
+ const novoArrayPets = pets.map((item) => {
+    return item.nome
+
+ })
+
+console.log(novoArrayPets)
+
+//b
+const novoArraySalsicha = pets.filter((item) => {
+return item.raca === "Salsicha"
+
+})
+
+console.log(novoArraySalsicha);
+
+//c
+
+
+
+//Exercício 2
+const produtos = [
+    { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+    { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+    { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+    { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+    { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+    { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
+    { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+    { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+    { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+    { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
+ ]
+
+ //a
+
+ const listaMercado = produtos.map((item) =>{
+return item.nome
+
+
+ } )
+console.log(listaMercado);
+
+//b
+
+
+
+
+//c
+
+const bebidas = produtos.filter((item) =>{
+return item.categoria === "Bebidas"
+
+})
+console.log(bebidas);
+
+//d 
+
+const marcaSabao = produtos.filter((item) => {
+    return item.nome.includes("Ypê")
+
+})
+
+console.log(marcaSabao);
+
+//e 
+
+
+
+
+
+//n1
+//C
+
+
+
+//n2
+
+//B e E
+
+
+
