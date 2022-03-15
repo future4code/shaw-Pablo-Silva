ff6c59bae0bc1f20d08fd4841b8a6fbe7c915c24
Pablo Andrade <pablofla14@hotmail.com>
2022-03-15 10:50:16 -0300

    revisao-js-postinho

--- modulo1/revisao-postinho/index.js
+++ modulo1/revisao-postinho/index.js
@@ -19,7 +19,7 @@ function checarIgualdade(a, b) {
 }
 console.log(checarIgualdade(1, 3));
 
-function verificaSeEMaior(a, b){
+function verificaSeEMaior(a, b) {
     return a > b
 }
 console.log(verificaSeEMaior(321, 2156));
@@ -47,11 +47,11 @@ const cadastro = () => {
     const senhaDoUsuario = prompt('Digite sua senha:')
     const nacionalidade = prompt('Digite sua nacionalidade:')
     usuario.push(nomeDoUsuario, anoDeNascimento, senhaDoUsuario, nacionalidade)
-    if(2022 - anoDeNascimento > 18 && senhaDoUsuario.length >= 6 && nacionalidade === "Brasileira"){
+    if (2022 - anoDeNascimento > 18 && senhaDoUsuario.length >= 6 && nacionalidade === "Brasileira") {
         return 'Prossiga com seu cadastro'
-        
 
-    }else{
+
+    } else {
         return 'Você não atende aos requistos mínimos para se cadastrar'
     }
 
@@ -66,16 +66,16 @@ console.log(cadastro());
 const login = () => {
     const senha = "labenu"
     const digiteSenha = prompt('Digite sua senha')
-    if(senha === digiteSenha){
+    if (senha === digiteSenha) {
         return 'Usuário logado'
 
-    }else{
+    } else {
         return 'Senha inválida'
 
     }
-}    
+}
 console.log(login());
- 
+
 
 
 
@@ -88,15 +88,15 @@ const primeiraDose = () => {
     const vacinaTomada = prompt('Digite o nome da vacina que você tomou:').toLowerCase()
     let tempo = ''
     let data = ''
-    if(vacinaTomada === 'Astrazenica' || vacinaTomada === 'Pfizer'){
+    if (vacinaTomada === 'Astrazenica' || vacinaTomada === 'Pfizer') {
         tempo = 90
         data = '12/06/2022'
-    
-    }else{
+
+    } else {
         tempo = 28
-        data = '11/04/2022'       
+        data = '11/04/2022'
     }
-    
+
     return `Olá ${seuNome}! A próxima dose da ${vacinaTomada} é daqui a ${tempo} dias. Compareça no posto na data ${data}.`
 
 }
@@ -112,10 +112,10 @@ const segundaDose = (nomeDoUsuario) => {
     const usuarios = [
         { nome: "Artur", imunizacao: "incompleta" },
         { nome: "Barbara", imunizacao: "incompleta" },
-        { nome: "Carlos", imunizacao: "incompleta" }    
+        { nome: "Carlos", imunizacao: "incompleta" }
     ]
 
-    return { ...usuarios, nomeDoUsuario, imunizacao: "completa"}  
+    return { ...usuarios, nomeDoUsuario, imunizacao: "completa" }
 
 
 }
@@ -127,12 +127,12 @@ const avisoAosAtrasados = () => {
     const usuarios = [
         { nome: "Artur", imunizacao: "incompleta" },
         { nome: "Barbara", imunizacao: "completa" },
-        { nome: "Carlos", imunizacao: "incompleta"}
+        { nome: "Carlos", imunizacao: "incompleta" }
     ]
-    for(let i = 0; i < usuarios.length; i++){
-       if (usuarios[i].imunizacao === "incompleta"){
-        console.log(`Olá ${usuarios[i].nome}! Sua imunização está ${usuarios[i].imunizacao}, por favor volte ao postinho para tomar a segunda dose.`)
-       }
+    for (let i = 0; i < usuarios.length; i++) {
+        if (usuarios[i].imunizacao === "incompleta") {
+            console.log(`Olá ${usuarios[i].nome}! Sua imunização está ${usuarios[i].imunizacao}, por favor volte ao postinho para tomar a segunda dose.`)
+        }
     }
 
 
@@ -181,7 +181,7 @@ const loginDesafio = () => {
 console.log(loginDesafio());
 
 const primeiraDoseDesafio = () => {
-//  Sua lógica aqui
+    //  Sua lógica aqui
 }
 console.log(primeiraDoseDesafio())
 const segundaDoseDesafio = (nomeDoUsuario) => {
